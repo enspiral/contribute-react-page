89793a2acceb4ed96dc6ad01ce4e29ce80735244
Daniel Lewis <dan@rootsystems.nz>
2018-05-25 14:03:56 +1200

    init app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import { config } from './config.js'
+import './App.css'
+
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <span>hello world</span>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const glob = require('glob')
+
+const extractCSS = new ExtractTextPlugin({ filename: './build/static/css/extracted.bundle.css' })
+
+module.exports = {
+  entry: {
+    'bundle.js': glob.sync('build/static/?(js|css)/main.*.?(js|css)').map(f => path.resolve(__dirname, f))
+  },
+  output: {
+    filename: 'build/static/js/bundle.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: extractCSS.extract({ // Instance 1
+          fallback: 'style-loader',
+          use: [ 'css-loader' ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJsPlugin(),
+    extractCSS
+  ]
+}
