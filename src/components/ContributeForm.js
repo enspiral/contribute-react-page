aef7d8f8521fc9097015e407bba86d147bdaade9
dazyang <dazyang@protonmail.com>
2018-06-07 15:11:37 +1200

    added more input fields to the form

--- src/components/ContributeForm.js
+++ src/components/ContributeForm.js
@@ -28,16 +28,44 @@ class ContributeForm extends Component {
   render () {
     return (
       <div className="contributeContainer">
-        <form className='form' onSubmit={this.handleSubmit}>
-          <input type='text' className='button' placeholder='Name' value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })} />
-          <input type='text' className='button' placeholder='Email' value={this.state.email} onChange={(e) => this.setState({ email: e.target.value })} />
-          <input type='number' className='button' placeholder='Amount' value={this.state.amount} onChange={(e) => this.setState({ amount: e.target.value })} />
-          <CardElement className='card' />
-          <input type='submit' className='button' value='CONTRIBUTE' />
-        </form>
+      <fieldset>
+        <legend className='card-only'>Pay with card</legend>
+          <form className='form' onSubmit={this.handleSubmit}>
+              <div className="row">
+                <div className="field">
+                  <label for="name">Name</label>
+                  <input id="name" className="input" type="text" placeholder="Your Name" required="" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })}/>
+                </div>
+              </div>
+              <div className="row">
+                <div className="field">
+                  <label for="email">Email</label>
+                  <input id="email" className="input" type="text" placeholder="janedoe@gmail.com" value={this.state.email} onChange={(e) => this.setState({ email: e.target.value })}/>
+                </div>
+                <div className="field">
+                  <label for="phone">Phone</label>
+                  <input id="phone" className="input" type="text" placeholder="(02X)123-4567" required=""/>
+                </div>
+              </div>
+                <div className="row">
+                  <div className="field">
+                    <label for="address">Address</label>
+                    <input id="address" className="input" type="text" placeholder="185 Berry St" required=""/>
+                    <label for="amount">Amount</label>
+                    <input id="amount" className="input" type="text" placeholder="$" value={this.state.amount} onChange={(e) => this.setState({ amount: e.target.value })}/>
+                  </div>
+                </div>
+                <label for="card">Card</label>
+                  <div id="card">
+                    <CardElement class='card'/>
+                  </div>
+            <button type="submit" data-tid="">Pay</button>
+          </form>
+        </fieldset>
       </div>
     )
   }
 }
 
 export default injectStripe(ContributeForm)
+
