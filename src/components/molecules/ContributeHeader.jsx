c1b490fd80db2925550c2f1b49400039e8242027
dazyang <dazyang@protonmail.com>
2019-05-02 16:21:12 +1200

    added conditional state in ContrubteHeader

--- src/components/molecules/ContributeHeader.jsx
+++ src/components/molecules/ContributeHeader.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function ContributeHeader (props) {
   const {amount} = props
+  const selectedAmount = amount ? amount : "Select Amount"
+  
   return (
     <div className='formHeader'>
-      <p>Contributing $<b>{amount}</b> to Ensprial</p>
+      <p>Contributing $<b>{selectedAmount}</b> to Ensprial</p>
     </div>
   )
 }
