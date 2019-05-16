1e7309f6d45d24953004e4ea1f102268bb935c25
dazyang <dazyang@protonmail.com>
2019-05-16 16:30:49 +1200

    updated terms message on the footer

--- src/components/molecules/InsertDetails.jsx
+++ src/components/molecules/InsertDetails.jsx
@@ -13,6 +13,9 @@ function InsertDetails (props) {
           <LastName lastname={lastname} onChange={updateDetails}/>
           <EmailAddress email={email} onChange={updateDetails}/>
           <CreditCardInfo />
+        <h6>By contributing funds to Enspiral you agree to {' '}
+          <a rel="noopener" alt="Terms of Service" href="https://handbook.enspiral.com/finances_variable_contributions.html">these terms.</a>{' '}  
+          Have problems? Get in touch – <a href="mailto:foundation@enspiral.com">foundation@enspiral.com</a></h6>
       </div>
     );
   }
