206b5b567f721d3cdf432c4601f6fb22a3ee18b6
dazyang <dazyang@protonmail.com>
2019-05-16 16:31:03 +1200

    added ThankYou component

---
+++ src/components/molecules/ThankYou.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ThankYou(props) {
+  return (
+    <div>
+      <h3>Thank you for supporting Enspiral!</h3>
+      <p>Your payment has been processed. We have sent you a confirmation
+       email.</p>
+    </div>
+  );
+}
+
+export default ThankYou
