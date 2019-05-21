b0b3a36f2a3efe8d2efc7fbb6c49dafa6cd0ae60
Daz C. Yang <daz@dazyang.local>
2019-05-21 12:03:37 +1200

    added loadingIcon and float button to the right

--- src/components/molecules/FormFooter.jsx
+++ src/components/molecules/FormFooter.jsx
@@ -2,21 +2,27 @@ import React from 'react'
 
 function FormFooter(props) {
   const {paymentStep, stepOne, stepTwo, processPayment, isLoading} = props
+  const positionRight = {
+    float: "right", 
+    marginLeft:"auto"
+  }
   return (
     <div>
       {paymentStep === 1 && <div className="formFooter">
-         <button onClick={stepOne} type="submit">Details &rarr;</button>
+         <button onClick={stepOne} type="submit" style={positionRight}>Details &rarr;</button>
       </div>}
-      {paymentStep === 2 && <div className="formFooter">
+      {paymentStep === 2 && 
+        <div className="formFooter">
         <button onClick={stepTwo} className='editBtn'>&larr; Edit Amount</button>
         <button onClick={processPayment} disabled={isLoading}>
           {!isLoading && "Confirm Payment"}
-          {isLoading && "Processing..."}
+          {isLoading && <img alt='' src='../../loadingIcon.gif'/>}
+          {isLoading && <span>Processing...</span>}
         </button>
       </div>}
-      {paymentStep === 3 && <div className="formFooter">
+      {/* {paymentStep === 3 && <div className="formFooter">
         <button>Questions? Contact us</button>
-      </div>}
+      </div>} */}
     </div>
     )
   }
