d37d68e0e03e38d96ffa1afc0133f204d0bb4550
Dan <dan.lewis@enspiral.com>
2019-07-11 13:08:47 +1200

    update error message

--- src/components/molecules/ErrorView.jsx
+++ src/components/molecules/ErrorView.jsx
@@ -6,8 +6,10 @@ function ErrorView(props) {
       <div className="ErrorView">
         <div>
           <h1>Oops!</h1>
-          Something has gone wrong {' '}
-          <a href="mailto:contact@enspiral.com">contact@enspiral.com</a>
+          <p>Something has gone wrong in processing your contribution :(</p>
+          <p>It is very likely your money is still in your account :)</p>
+          <p>If not please get in touch at: <a href="mailto:contact@enspiral.com">contact@enspiral.com</a></p>
+          <p>Or</p>
           <button onClick={()=>window.location.reload(true)}>Try again</button>
         </div>
       </div>
